e08acbd92e8471d54189d6e45fd474a728407186
nikglavic <115024775+nikglavic@users.noreply.github.com>
2024-03-14 19:25:34 +0100

    Update taskRouter

--- routes/api/taskRouter.js
+++ routes/api/taskRouter.js
@@ -3,4 +3,11 @@ let router = express.Router();
 
 let taskController = require("../../controllers/taskController.js");
 
+router.post("/list", taskController.list);
+router.post("/create", taskController.create);
+router.post("/updateStatus", taskController.updateStatus);
+router.post("/delete", taskController.delete);
+router.post("/points", taskController.points);
+router.post("/my", taskController.my);
+
 module.exports = router;
